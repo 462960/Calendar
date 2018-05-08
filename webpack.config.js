81d870901b1ac9aac4ea548696443ca4ca6aed07
Vladimir <462960@gmail.com>
2018-05-08 08:22:42 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const config = {
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-                exclude: /(node_modules|bower_components)/,   asasasas
+                exclude: /(node_modules|bower_components)/,
           },
             {
       test: /\.scss$/,
